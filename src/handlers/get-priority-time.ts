c3e474bb21e55b0c80a511d4a294480244fef810
shiv810 <test@example.com>
2025-05-06 11:18:30 -0400

    fix: remove paraphrasing comments & apiUrl -> basetenApiUrl

--- src/handlers/get-priority-time.ts
+++ src/handlers/get-priority-time.ts
@@ -14,7 +14,7 @@ export async function getPriorityTime(
   issueDescription: string,
   issueTitle: string,
   basetenApiKey: string,
-  apiUrl: string = "https://model-yqvr8mjw.api.baseten.co/development/predict"
+  basetenApiUrl: string = "https://model-yqvr8mjw.api.baseten.co/development/predict"
 ): Promise<PriorityTimeEstimate> {
   const headers = {
     "Content-Type": "application/json",
@@ -24,7 +24,7 @@ export async function getPriorityTime(
     title: issueTitle,
     description: issueDescription,
   });
-  const response = await fetch(apiUrl, {
+  const response = await fetch(basetenApiUrl, {
     method: "POST",
     headers: headers,
     body: body,
@@ -34,13 +34,12 @@ export async function getPriorityTime(
   }
   const data = (await response.json()) as PriorityTimeResponse;
   if (!data) {
-    throw new Error("Error: No data returned from Baseten API");
+    throw new Error("No data returned from Baseten API");
   }
   const time = data.estimated_time;
   const priority = data.priority;
-  console.log("Data from Baseten API: ", data);
   if (!time || !priority) {
-    throw new Error("Error: No time or priority returned from Baseten API");
+    throw new Error("No time or priority returned from Baseten API");
   }
   return {
     time: time,
@@ -51,12 +50,12 @@ export async function getPriorityTime(
 export function getPricing(base: number, time: number, priorityLabel: string): Decimal {
   const priority = RegExp(/\d+/).exec(priorityLabel)?.[0];
   if (!priority) {
-    throw new Error("Error: No priority value found");
+    throw new Error("No priority value found");
   }
   const priorityValue = new Decimal(priority).div(10);
   return new Decimal(base)
     .mul(1000)
     .mul(time * 0.125)
     .mul(priorityValue)
-    .toDecimalPlaces(2); // 0.125 is essentially 1/5 of a cent per minute
+    .toDecimalPlaces(2);
 }
